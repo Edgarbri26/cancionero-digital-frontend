5d989c3f88b1e405e9d3f7297a429bc2a540d174
Edgarbriceno26@outlook.es <Edgarbriceno26@outlook.es>
2025-12-30 16:36:59 -0400

    feat: enable imageService for Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,14 +12,11 @@ import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: vercel({
+    imageService: true
+  }),
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()]
-  },
-
-
-
-
-  // integrations: [react()]
+  }
 });
