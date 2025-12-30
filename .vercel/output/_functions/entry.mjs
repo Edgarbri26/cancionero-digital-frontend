773a50f144fa3eae714fed49accd6296c9eac9ff
Edgarbriceno26@outlook.es <Edgarbriceno26@outlook.es>
2025-12-30 15:54:23 -0400

    feat: Add debug page, update Vercel build output, and remove pnpm-lock.yaml.

--- .vercel/output/_functions/entry.mjs
+++ .vercel/output/_functions/entry.mjs
@@ -1,26 +1,28 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_DuIYPZQY.mjs';
-import { manifest } from './manifest_DtzZ_j9o.mjs';
+import { manifest } from './manifest_CFAPbD7N.mjs';
 
 const serverIslandMap = new Map();;
 
 const _page0 = () => import('./pages/_image.astro.mjs');
 const _page1 = () => import('./pages/404.astro.mjs');
 const _page2 = () => import('./pages/add-song.astro.mjs');
-const _page3 = () => import('./pages/edit-song/_id_.astro.mjs');
-const _page4 = () => import('./pages/login.astro.mjs');
-const _page5 = () => import('./pages/search/_search_.astro.mjs');
-const _page6 = () => import('./pages/songs/_id_.astro.mjs');
-const _page7 = () => import('./pages/index.astro.mjs');
+const _page3 = () => import('./pages/debug.astro.mjs');
+const _page4 = () => import('./pages/edit-song/_id_.astro.mjs');
+const _page5 = () => import('./pages/login.astro.mjs');
+const _page6 = () => import('./pages/search/_search_.astro.mjs');
+const _page7 = () => import('./pages/songs/_id_.astro.mjs');
+const _page8 = () => import('./pages/index.astro.mjs');
 const pageMap = new Map([
     ["node_modules/astro/dist/assets/endpoint/generic.js", _page0],
     ["src/pages/404.astro", _page1],
     ["src/pages/add-song.astro", _page2],
-    ["src/pages/edit-song/[id].astro", _page3],
-    ["src/pages/login.astro", _page4],
-    ["src/pages/search/[search].astro", _page5],
-    ["src/pages/songs/[id].astro", _page6],
-    ["src/pages/index.astro", _page7]
+    ["src/pages/debug.astro", _page3],
+    ["src/pages/edit-song/[id].astro", _page4],
+    ["src/pages/login.astro", _page5],
+    ["src/pages/search/[search].astro", _page6],
+    ["src/pages/songs/[id].astro", _page7],
+    ["src/pages/index.astro", _page8]
 ]);
 
 const _manifest = Object.assign(manifest, {
@@ -31,7 +33,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "1521e164-1b81-4b69-9a44-c2677c8e315b",
+    "middlewareSecret": "17b4bf6a-723a-49c7-9de7-f741e6bb1bf0",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
